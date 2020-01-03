609faa5a051b545082312cf3339e38aa566ca861
alexiosdev <mobileapps.itbk@gmail.com>
2020-01-03 17:14:20 +0700

    update api

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php 
-$arrayData = array("auxiliary_verbs","mixed_comparisons","conjunctions","conditional_sentences","gerunds","infinitives","modal_verbs","imperative","interrogative_pronouns","relative_pronoun","mood","must_have_to","narration","nouns","participle_phrases","passive_voice","phrasal_verbs","quantifier","adjectives_as_nouns","relative_clauses","reported_speech","sentence_patterns","subject_and_verb_agreement","mixed_subjunctive","question_tag","measures","regular_and_irregular_verbs","verbal","verbs");
+$arrayData = array("have_has_had","adjectives","adverbs","adjectives_and_adverbs","am_is_are","were_was","there_is_there_are","do_does_did_done","auxiliary_verbs","mixed_comparisons","conjunctions","conditional_sentences","gerunds","infinitives","modal_verbs","imperative","interrogative_pronouns","relative_pronoun","mood","must_have_to","narration","nouns","participle_phrases","passive_voice","phrasal_verbs","quantifier","adjectives_as_nouns","relative_clauses","reported_speech","sentence_patterns","subject_and_verb_agreement","mixed_subjunctive","question_tag","measures","regular_and_irregular_verbs","verbal","verbs");
 foreach ($arrayData as $value) {
 	mkdir($value); 
 }
